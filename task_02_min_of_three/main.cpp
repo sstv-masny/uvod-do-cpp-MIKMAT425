ef6e6c7d92a405de950f246659faf1fe09dc8809
Matúš Mikolajčík <mikolajcikmatus9@gmail.com>
2026-06-02 12:04:30 +0200

    s

--- task_02_min_of_three/main.cpp
+++ task_02_min_of_three/main.cpp
@@ -5,6 +5,6 @@ int main() {
     std::cin.tie(nullptr);
     long long a,b,c;
     if (!(std::cin >> a >> b >> c)) return 0;
-    // TODO: print the minimum of a, b, c
+       std:: cout << std::min(a,b,c);
     return 0;
 }
